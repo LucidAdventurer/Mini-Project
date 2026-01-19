9f4fb7598c4918b0bb5d6f32019b3e61af018fd7
Antony101Thomas <149679195+Antony101Thomas@users.noreply.github.com>
2026-01-19 09:27:01 +0530

    removed hi

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 /*
  * ========================================
- * PTA GLOBAL CONFIGURATION - FIXED VERSION hi
+ * PTA GLOBAL CONFIGURATION - FIXED VERSION
  * File: config.php
  *
  * Purpose: Core application setup, including error reporting, database connection,
