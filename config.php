2cb3b04563cdc60aa924940d0f6be4f1822ecf5b
Justin <justinkurian1234@gmail.com>
2026-01-19 09:28:29 +0530

    added version

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 /*
  * ========================================
- * PTA GLOBAL CONFIGURATION - FIXED VERSION
+ * PTA GLOBAL CONFIGURATION - FIXED VERSION 1.0
  * File: config.php
  *
  * Purpose: Core application setup, including error reporting, database connection,
